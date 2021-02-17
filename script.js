6c043c4e5e706348ba845252121d9e4cced1cf6b
matrosov <egor.matrosov@gmail.com>
2021-02-17 10:20:45 +0700

    lesson1; added jpg

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url)
-    const body = req.url === '/CSS/style.css'
-        ? fs.readFileSync('./public/CSS/style.css', 'utf8')
-        : fs.readFileSync('./public/index.html', 'utf8')
+
+    let body = null
+    try {
+        const ext = req.url.split('.')[1]
+        const isSvg = ext === 'svg';
+        if (isSvg) {
+            res.setHeader('Content-Type', 'image/svg+xml');
+        }
+        body = fs.readFileSync(`./public${req.url}`)
+    } catch (err) {
+        body = fs.readFileSync(`./public/index.html`)
+    }
+    //     ? fs.readFileSync('./public/CSS/style.css', 'utf8')
+    //     : fs.readFileSync('./public/index.html', 'utf8')
     res.end(body)
 })
 
